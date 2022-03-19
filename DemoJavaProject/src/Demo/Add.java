b07546cb43a466f9268e3d55c87a69ef4a060f74
sunil4161 <sunilbiradar109@gmail.com>
2022-03-19 22:42:43 +0530

    Intial Draft

---
+++ DemoJavaProject/src/Demo/Add.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		int b=4;//
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
